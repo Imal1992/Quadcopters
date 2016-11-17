c30fe61097571d8ff97c11eb637c43760bc824e1
ඉමාල් තියුනුවන්(Imal Thiunuwan) <ithiunuwan@gmail.com>
2016-11-17 15:35:22 +0530

    add addQuadcopter function and register page

--- QuadCopters/routes/index.js
+++ QuadCopters/routes/index.js
@@ -18,9 +18,40 @@ router.get('/quadcopters', function(req, res) {
     });
 });
 
-/* GET home page. */
-router.get('/quadcopters', function(req, res, next) {
-  res.render('quadcopters', { title: 'Quadcopters' });
+/* GET register page. */
+router.get('/reg_quadcopter', function(req, res, next) {
+  res.render('reg_quadcopter', { title: 'Register Quadcopters' });
 });
+
+
+/* POST to Add Quadcopter Service */
+router.post('/quadcopters', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var name = req.body.name;
+    var model = req.body.model;
+
+    // Set our collection
+    var collection = db.get('reg_data');
+
+    // Submit to the DB
+    collection.insert({
+        "name" : name,
+        "model" : model
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect('/reg_quadcopter');
+        }
+    });
+});
+
 
 module.exports = router;
